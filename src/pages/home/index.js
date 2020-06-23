a44d47425467fb2fe95b0f337d0cad425d6c72d8
yusimgo <67082871+yusimgo@users.noreply.github.com>
2020-06-23 15:20:02 +0700

    update folder home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 
 import useStyles from "./styles";
 
@@ -13,8 +13,6 @@ export default function Registrasi() {
     <>
       <div className={classes.daftarBlock}>
         <div classNmae={classes.menuBarisSatu}>
-          <img className={classes.logoBox} src={MenuArtikel} alt="" />
-
           <a href="https://muslimgoonline.com/yuk-ngaji/artikel">
             <img className={classes.logoBox} src={MenuArtikel} alt="" />
           </a>
@@ -30,6 +28,37 @@ export default function Registrasi() {
           <a href="https://muslimgoonline.com/yuk-ngaji/jualan">
             <img className={classes.logoBox} src={MenuArtikel} alt="" />
           </a>
+        </div>
+      </div>
+    </>
+  );
+}
+ */
+
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import useStyles from "./styles";
+
+export default function Registrasi() {
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.daftarBlock}>
+        <div className={classes.daftarBox}>
+          <div className={classes.logoBox}>{/* untuk logo */}</div>
+          <Container maxWidth="xs">
+            <Paper className={classes.paper}>
+              <Typography variant="h5" component="h1" className={classes.title}>
+                Buat Akun Baru
+              </Typography>
+              <Grid container className={classes.button}>
+                <Typography>Menu Artikel</Typography>
+              </Grid>
+            </Paper>
+          </Container>
         </div>
       </div>
     </>
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
+/* import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(theme => ({
   daftarBox: {
@@ -12,5 +12,51 @@ export default makeStyles(theme => ({
     flexDirection: "column",
     padding: "10px",
     alignItems: "center"
+  }
+}));
+ */
+
+import { makeStyles } from "@material-ui/core/styles";
+import bg from "../../images/crop.jpg";
+
+export default makeStyles(theme => ({
+  daftarBlock: {
+    backgroundImage: `url(${bg})`,
+    width: "100%",
+    height: 320,
+    borderRadius: "0 0 100% 100%",
+    position: "relative",
+    "&;before": {
+      content: '""',
+      opacity: "-9",
+      width: "100%",
+      height: 320,
+      position: "absolute",
+      top: 0,
+      left: 0,
+      borderRadius: "0 0 100% 100%",
+      zIndex: 1
+    }
+  },
+  daftarBox: {
+    position: "relative",
+    height: "100%",
+    zIndex: 2,
+    display: "flex",
+    alignItems: "center",
+    flexFlow: "column nowrap"
+  },
+  logoBox: {
+    width: 282,
+    height: 69,
+    margin: "30px auto 20px"
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: theme.spacing(3)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    boxShadow: "none"
   }
 }));
