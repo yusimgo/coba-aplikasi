e0ae6a941bfa2caca64eb6c8ec7039d137855fc3
yusimgo <67082871+yusimgo@users.noreply.github.com>
2020-06-24 17:50:27 +0700

    halaman chat

--- src/pages/private/chat/list.js
+++ src/pages/private/chat/list.js
@@ -9,14 +9,48 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
+import { useHistory } from "react-router-dom";
+
 export default function ChatList() {
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6">Halaman Chat</Typography>
-        <IconButton>
+        <IconButton
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
           <MoreVertIcon />
         </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              handleClose();
+              history.push("/pengaturan");
+            }}
+          >
+            Profile
+          </MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
