9b2519c20d8485eea9b9cfb71b0ebfc385c26c7d
yusimgo <67082871+yusimgo@users.noreply.github.com>
2020-06-24 14:50:21 +0700

    lading update

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+import Button from "@material-ui/core/Button";
+import { auth } from "../../../components/FirebaseProvider";
+
+import { Link } from "react-router-dom";
+
 export default function Landing() {
-  return <h1>Halaman Landing</h1>;
+  return <>
+  <Button component={Link} to="/daftar">
+    Daftar
+  </Button>
+
+  <Button component={Link} to="/Login">
+    Login
+  </Button>
+</>
 }
