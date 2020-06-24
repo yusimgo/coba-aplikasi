ed4935631780d30cbbb5b824b0c0186aed320add
yusimgo <67082871+yusimgo@users.noreply.github.com>
2020-06-24 14:04:59 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
+import Landing from "./pages/landing";
 import Registrasi from "./pages/registrasi";
 import Login from "./pages/login";
 import LupaPassword from "./pages/lupa-password";
 import NotFound from "./pages/404";
 import Private from "./pages/private";
 import PrivateRoute from "./components/PrivateRoute";
-
+import Chat from "./pages/private/chat";
 import Home from "./pages/home";
 
 import FirebaseProvider from "./components/FirebaseProvider";
@@ -19,11 +20,12 @@ export default function App() {
       <FirebaseProvider>
         <Router>
           <Switch>
-            <Route path="/" exact component={Registrasi} />
+            <Route path="/" exact component={Landing} />
+            <Route path="/registrasi" component={Registrasi} />
             <Route path="/login" component={Login} />
             <Route path="/lupa-password" component={LupaPassword} />
+            <PrivateRoute path="/chat" component={Chat} />
             <PrivateRoute path="/home" component={Home} />
-            <PrivateRoute path="/chat" component={Private} />
             <PrivateRoute path="/pengaturan" component={Private} />
 
             <Route component={NotFound} />
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,103 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+import useStyles from "./styles";
 
 export default function Login() {
-  return <h1>Halaman Login</h1>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.daftarBlock}>
+        <div className={classes.daftarBox}>
+          <div className={classes.logoBox}>{/* untuk logo */}</div>
+          <Container maxWidth="xs">
+            <Paper className={classes.paper}>
+              <Typography variant="h5" component="h1" className={classes.title}>
+                Buat Akun Baru
+              </Typography>
+              <form>
+                <TextField
+                  id="nama"
+                  name="nama"
+                  label="Nama"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
+                <TextField
+                  id="email"
+                  name="email"
+                  label="Email"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
+                <TextField
+                  id="notelp"
+                  type="number"
+                  name="notelp"
+                  label="Notelp"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
+                <TextField
+                  id="password"
+                  type="password"
+                  name="password"
+                  label="Password"
+                  autoComplete="new-password"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
+                <TextField
+                  id="ulangi_password"
+                  type="password"
+                  name="ulangi_password"
+                  label="Ulangi Password"
+                  autoComplete="new-password"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
+                <Grid container className={classes.button}>
+                  <Grid item xs>
+                    <Button
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      size="large"
+                    >
+                      Daftar
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      disable={isSubmitting}
+                    >
+                      Login
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Paper>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
 }
