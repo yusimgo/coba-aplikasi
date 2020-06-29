c2f5fd6f1ab7cc911aaf972f4bec72c4df6a87c9
yusimgo <67082871+yusimgo@users.noreply.github.com>
2020-06-29 13:42:18 +0700

    update siang

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 
 import Artikel from "../../images/article-white2.png";
 import Event from "../../images/event-white2.png";
